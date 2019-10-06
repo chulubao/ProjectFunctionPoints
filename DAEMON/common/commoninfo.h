0d6720192c75b28ccede2b8795ef6f5d4ee28e61
chulujian <chulujian@126.com>
2019-10-07 00:33:30 +0800

    删除了DAEMON

--- DAEMON/common/commoninfo.h
+++
@@ -1,7 +0,0 @@
-#ifndef COMMONINFO_H
-#define COMMONINFO_H
-namespace  nsCommonInfo{
-extern char *get_current_date(char *str);
-extern void  getAgentID(char *pcAgentID);
-}
-#endif // COMMONINFO_H
--- DAEMON/ui/uiProcess/xmltool.h
+++
@@ -1,8 +0,0 @@
-#ifndef XMLTOOL_H
-#define XMLTOOL_H
-#include <QString>
-extern void createXml(QString fromText,QString toText,QString typeText,\
-               QString elemName,QString elemAttributeText,QString userRoleText,QString userNameText,QString passwordText,\
-               QString &outResult);
-extern bool  parseXmlMsg(QString  xmlMsg);
-#endif // XMLTOOL_H
